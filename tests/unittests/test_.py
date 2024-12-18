a56637ab87db030fb7720341979978761ff504ea
Limespy <37109901+Limespy@users.noreply.github.com>
2024-12-18 16:30:12 +0200

    v0.0.1

---
+++ tests/unittests/test_.py
@@ -0,0 +1,9 @@
+import limeinstall
+import pytest
+# ======================================================================
+parametrize = pytest.mark.parametrize
+# ======================================================================
+class Test_:
+
+    def test_(self):
+        ...
